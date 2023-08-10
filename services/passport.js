ad727cca1db8f4eca5c4e7293f407a9f352c6419
koukoudo <nikanasta15@gmail.com>
2023-08-10 18:15:13 +0100

    setup mongodb

--- services/passport.js
+++ services/passport.js
@@ -1,12 +1,30 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    
-})
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleID: profile.id })
+            .then(existingUser => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({ googleID: profile.id })
+                        .save()
+                        .then(user => {
+                            done(null, user);
+                        })
+                }
+            })
+    })
 );
