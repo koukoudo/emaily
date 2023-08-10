aa55c3b27911f26995c1563baa2a6b7957d33106
koukoudo <nikanasta15@gmail.com>
2023-08-10 11:24:20 +0100

    hello world

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('./services/passport');
 
-const app = express();app.get('/',(req, res) => {
-    res.send({ hi: 'there' });
+const app = express();
 
-});
+console.log("HELLO WORLD");
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
